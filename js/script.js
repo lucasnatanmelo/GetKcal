02875c86cbb3fa8dbc59f4751d97c10e1d62e5e6
Lucas Natan de Almeida Melo <lucasnatan.dev@gmail.com>
2022-05-23 19:08:13 -0400

    add imc indicators and input conditionals

--- js/script.js
+++ js/script.js
@@ -9,12 +9,14 @@ function handleSubmit(event) {
   const weight = getInputNumberValue('weight');
   const height = getInputNumberValue('height');
   const activityLevel = getSelectedValue('activity_level');
+  const imc = calculateImc(gender, weight, height);
 
   const tmb = Math.round(
     gender === 'female'
       ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 * age))
       : (66 + (13.7 * weight) + (5 * height) - (6.8 * age))
-  );
+);
+
   const maintenance = Math.round(tmb * Number(activityLevel));
   const loseWeight = maintenance - 450;
   const gainWeight = maintenance + 450;
@@ -25,16 +27,19 @@ function handleSubmit(event) {
     <div class="result-content">
       <ul>
         <li>
-          Seu metabolismo basal é de <strong>${tmb} calorias</strong>.
+          Seu índice de Massa Corporal indica <strong>${imc}</strong>
         </li>
         <li>
-          Para manter o seu peso você precisa consumir em média <strong>${maintenance} calorias</strong>.
+          Seu metabolismo basal é de <strong>${tmb} calorias</strong>
         </li>
         <li>
-          Para perder peso você precisa consumir em média <strong>${loseWeight} calorias</strong>.
+          Para manter o seu peso você precisa consumir em média <strong>${maintenance} calorias</strong>
         </li>
         <li>
-          Para ganhar peso você precisa consumir em média <strong>${gainWeight} calorias</strong>.
+          Para perder peso você precisa consumir em média <strong>${loseWeight} calorias</strong>
+        </li>
+        <li>
+          Para ganhar peso você precisa consumir em média <strong>${gainWeight} calorias</strong>
         </li>
       </ul>
     </div>
@@ -52,4 +57,45 @@ function getSelectedValue(id) {
 
 function getInputNumberValue(id) {
   return Number(document.getElementById(id).value);
+}
+
+
+function calculateImc(gender, weight, height){
+  let heightToMeter = height/100;
+  let imcPararamether = ((weight)/(heightToMeter*heightToMeter));
+  if((gender === 'male')){
+    if (imcPararamether >= 40){
+      return 'Obesidade Grau III Mórbida';
+    }
+    if((imcPararamether >= 30) && (imcPararamether < 40)){
+      return 'Obesidade Grau II Moderada';
+    }
+    if((imcPararamether >= 25) && (imcPararamether < 30)){
+      return 'Obesidade Grau I leve';
+    }
+    if((imcPararamether >= 20) && (imcPararamether < 25)){
+      return 'Peso Normal';
+    }
+    if((imcPararamether < 20)){
+      return 'Peso abaixo do normal';
+    }
+  } else{
+    if((gender === 'female')){
+    if (imcPararamether >= 40){
+      return 'Obesidade Grau III Mórbida';
+    }
+    if((imcPararamether >= 30) && (imcPararamether < 40)){
+      return 'Obesidade Grau II Moderada';
+    }
+    if((imcPararamether >= 24) && (imcPararamether < 30)){
+      return 'Obesidade Grau I leve';
+    }
+    if((imcPararamether >= 19) && (imcPararamether < 24)){
+      return 'Peso Normal';
+    }
+    if((imcPararamether < 19)){
+      return 'Peso abaixo do normal';
+    }
+  }
+}
 }
